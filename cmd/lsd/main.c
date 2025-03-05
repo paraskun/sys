e5b0053820e1466a27c31ea7c61fa242f8f2de41
Ivan Paraskun <ig.paraskun@gmail.com>
2025-03-05 22:44:55 +0700

    upd: cmd/lsd: formatting

--- cmd/lsd/main.c
+++ cmd/lsd/main.c
@@ -1,88 +1,92 @@
 #include <dirent.h>
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
-int error(const char* path)
+/// @brief Print error and return.
+/// @param path error point
+static int error(const char* path)
 {
-  fprintf(stderr, "error: %s: %s\n", path, strerror(errno));
-  return -1;
+    fprintf(stderr, "error: %s: %s\n", path, strerror(errno));
+    return -1;
 }
 
-int isDir(const char* path)
+/// @brief Determine if there is directory at the specified path.
+/// @param path exploration point
+static int isDir(const char* path)
 {
-  struct stat s;
+    struct stat s;
 
-  if (stat(path, &s) == -1)
-    return -1;
+    if (stat(path, &s) == -1)
+        return -1;
 
-  return S_ISDIR(s.st_mode) ? 1 : 0;
+    return S_ISDIR(s.st_mode) ? 1 : 0;
 }
 
 int main(int argc, char** argv)
 {
-  char path[257] = ".";
+    char path[257] = ".";
 
-  if (argc > 1)
-    strncpy(path, argv[1], sizeof(path));
+    if (argc > 1)
+        strncpy(path, argv[1], sizeof(path));
 
-  DIR* odir = opendir(path);
+    DIR* odir = opendir(path);
 
-  if (!odir)
-    return error(path);
+    if (!odir)
+        return error(path);
 
-  struct dirent* ent = 0;
+    int i = strlen(path); // outer dir path offset
+    int j = 0;            // inner dir path offset
 
-  int i = strlen(path);
-  int j = 0;
+    struct dirent* ent = 0;
 
-  while ((ent = readdir(odir)))
-  {
-    if (!strcmp(ent->d_name, "..") || !strcmp(ent->d_name, "."))
-      continue;
-
-    j = i + sprintf(path + i, "/%s", ent->d_name);
-
-    if (!isDir(path))
-      continue;
-
-    DIR* idir = opendir(path);
-
-    if (idir)
+    while ((ent = readdir(odir)))
     {
-      unsigned char has = 0;
-
-      while ((ent = readdir(idir)))
-      {
         if (!strcmp(ent->d_name, "..") || !strcmp(ent->d_name, "."))
-          continue;
+            continue;
 
-        sprintf(path + j, "/%s", ent->d_name);
+        j = i + sprintf(path + i, "/%s", ent->d_name);
 
-        if (isDir(path))
+        if (!isDir(path))
+            continue;
+
+        DIR* idir = opendir(path);
+
+        if (!idir)
         {
-          has = 1;
-          break;
+            error(path);
+            continue;
         }
-      }
 
-      if (has)
-      {
-        path[j + 1] = '\0';
-        printf("%s\n", path);
-      }
+        bool has = false;
+
+        while ((ent = readdir(idir)))
+        {
+            if (!strcmp(ent->d_name, "..") || !strcmp(ent->d_name, "."))
+                continue;
+
+            sprintf(path + j, "/%s", ent->d_name);
+
+            if (isDir(path))
+            {
+                has = true;
+                break;
+            }
+        }
+
+        if (has)
+        {
+            path[j + 1] = '\0';
+            printf("%s\n", path);
+        }
+
+        closedir(idir);
     }
-    else
-    {
-      error(path);
-    }
 
-    closedir(idir);
-  }
+    closedir(odir);
 
-  closedir(odir);
-
-  return 0;
+    return 0;
 }
